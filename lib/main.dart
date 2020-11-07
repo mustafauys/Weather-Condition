57fcd4fb5fc29f498e4067579246b812a7963370
Mustafa <18701031@mersin.edu.tr>
2020-11-07 23:46:00 +0300

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:notebasket/utils/database_helper.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    var dd = DatabaseHelper();
+
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        
       ),
       home: Text(""),
     );
