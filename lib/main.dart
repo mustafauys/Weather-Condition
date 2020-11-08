e3b5e31f81e820b9a8c2208e6f3ce4e22aef0513
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2020-11-08 23:41:32 +0300

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     var dd = DatabaseHelper();
 
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Note Basket',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
