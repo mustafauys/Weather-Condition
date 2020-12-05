74f326057173ab0eded4c4edc43d61e4723e87fe
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2020-12-05 23:47:27 +0300

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
