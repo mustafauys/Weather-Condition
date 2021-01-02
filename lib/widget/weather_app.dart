be81b63c28a68e8d9ff5bfd3aeaa43887ea2693c
Mustafa <18701031@mersin.edu.tr>
2021-01-02 23:34:37 +0300

    Preparing the Interface

--- lib/widget/weather_app.dart
+++ lib/widget/weather_app.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:weather/widget/sehir_sec.dart';
 
 import 'hava_durumu_resim.dart';
 import 'location.dart';
 import 'max_min_sicaklik.dart';
 import 'son_guncelleme.dart';
 
+// ignore: must_be_immutable
 class WeatherApp extends StatelessWidget {
+
+  String kullanicininSectigiSehir = "Ankara";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Weather App"),
-      actions: <Widget>[
-        IconButton(icon: Icon(Icons.search), onPressed: () {})
-      ],
+      appBar: AppBar(
+        title: Text("Weather App"),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () async{
+                 kullanicininSectigiSehir = await Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SehirSecWidget()));
+              })
+        ],
       ),
       body: Center(
         child: ListView(
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Center(child: LocationWidget()),
+              child: Center(child: LocationWidget(secilenSehir: kullanicininSectigiSehir,)),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
