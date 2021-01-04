682cb6fe00451348c0dc80f91be4e93ab01d46af
Mustafa <18701031@mersin.edu.tr>
2021-01-04 23:26:57 +0300

    Creating the Bloc Structure for Weather

--- lib/blocs/weather/bloc/weather_bloc.dart
+++ lib/blocs/weather/bloc/weather_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:weather/models/weather.dart';
 import 'bloc.dart';
 
 
@@ -11,6 +12,15 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    
+    if (event is FetchWeatherEvent) {
+      
+      yield WeatherLoadingState();
+      try {
+
+        yield WeatherLoadedState(weather: Weather());
+      } catch(_){
+        yield WeatherErrorState();
+      }
+    }
   }
 }
