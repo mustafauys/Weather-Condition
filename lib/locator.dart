2ee0daaf84e0e7d8dc344e96d869d3da5387d8fb
Mustafa <18701031@mersin.edu.tr>
2021-01-06 23:54:46 +0300

    Preparing the Api Resource and Creating the Model Class

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 
+import 'data/weather_api_client.dart';
 import 'data/weather_repository.dart';
 
 GetIt locator = GetIt();
@@ -7,6 +8,6 @@ GetIt locator = GetIt();
 void setupLocator(){
 
   locator.registerLazySingleton(() => WeatherRepository());
-  
+  locator.registerLazySingleton(() => WeatherApiClient());
 
 }
