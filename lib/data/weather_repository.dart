2a8ad937e397ca9531292915472f8680882d61ee
Mustafa <18701031@mersin.edu.tr>
2021-01-06 23:54:50 +0300

    Preparing the Api Resource and Creating the Model Class

--- lib/data/weather_repository.dart
+++ lib/data/weather_repository.dart
@@ -1,10 +1,14 @@
+import 'package:weather/data/weather_api_client.dart';
+import 'package:weather/locator.dart';
 import 'package:weather/models/weather.dart';
 
 class WeatherRepository {
 
+  WeatherApiClient weatherApiClient = locator<WeatherApiClient>();
+
   // ignore: missing_return
   Future<Weather> getWeather(String sehir) async {
-
+    
   }
 
 }
