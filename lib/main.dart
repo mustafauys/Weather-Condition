885c3ce3ffe5ad8a8eb6a4d86d3ef028a63f5b24
Mustafa <18701031@mersin.edu.tr>
2021-01-11 23:54:47 +0300

    Changing the Theme According to the Weather

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/blocs/Tema/bloc/bloc.dart';
+import 'package:weather/blocs/Tema/bloc/tema_state.dart';
 import 'package:weather/locator.dart';
 import 'blocs/weather/bloc/bloc.dart';
 import 'widget/weather_app.dart';
 
 void main() {
   setupLocator();
-  runApp(MyApp());
+  runApp(
+    BlocProvider<TemaBloc>(
+      builder: (context) => TemaBloc(),
+      child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return BlocBuilder(
+      bloc: BlocProvider.of<TemaBloc>(context),
+      builder: (context, TemaState state) => MaterialApp(
+        title: 'Weather App',
+        debugShowCheckedModeBanner: false,
+        theme: (state as UygulamaTemasi).tema,
+        home: BlocProvider<WeatherBloc>(
+            builder: (context) => WeatherBloc(), child: WeatherApp()),
       ),
-      home: BlocProvider<WeatherBloc>(
-        builder: (context) => WeatherBloc(),
-        child: WeatherApp()),
     );
   }
 }
